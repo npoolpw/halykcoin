aaddb8f5ecc4e816343f0c89cca1f1319a4e8367
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-04-16 07:54:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,5 +17,5 @@ var themeCss = "themes/default-theme.css";
 var networkStat = [
         ["npool.pw/halykcoin", "http://ca.npool.pw:2250"],
         ["halykcoin.hol.es", "http://140.82.32.6:7118"],
-        ["pool2stat.halykcoin.org", "http://poolv2.halykcoin.org:8113"]
+        ["pool.halykcoin.org", "http://pool.halykcoin.org:8113"]
 ];
